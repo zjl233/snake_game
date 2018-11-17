303b4938eeb5e3791596f8c7b4cc0a1d37502e6f
zjl233 <42487243+zjl233@users.noreply.github.com>
2018-11-17 18:49:26 +0800

    📦 comment log for product

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 # for debug in linux terminal
 def log(*args, **kwargs):
-    from datetime import datetime
-    dt = datetime.now()
-    with open('log.txt', 'a', encoding='utf-8') as f:
-        print(dt, *args, file=f, **kwargs)
+    ...
+    # from datetime import datetime
+    # dt = datetime.now()
+    # with open('log.txt', 'a', encoding='utf-8') as f:
+    #    print(dt, *args, file=f, **kwargs)
 
 
 def cycle(n, mn, mx):
